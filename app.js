0100d50098f59ae188d958c0ff9963884f00b1ef
chokoreetosan <mallepalliabhishek@gmail.com>
2019-04-22 17:04:41 -0700

    finished everything but states

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // TODO
+// var Cucumbers = () =>(<span>Cucumbers</span>);
+
+// var Kale = () =>(<span>Kale</span>);
+
+// var list = ['Kale', 'Cheese', 'Eggplant'];
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+ //   props.list = ['Kale', 'Cheese', 'Eggplant'];
+
+  }
+  render () {
+   return (
+    <li>{this.props.item}</li>
+   );
+  }
+};
+
+var GroceryList = (props) => (
+  <ul>
+    {props.list.map(item =>
+    <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList list={['Kale', 'Cheese', 'Eggplant']}/>, document.getElementById('app'));
+
